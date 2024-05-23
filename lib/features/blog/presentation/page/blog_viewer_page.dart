5a7ebfa5c6f9736428ee8fbdcd0f078c29eb1df1
Shubham Rai <w3villa@W3VILLAs-MacBook-Pro.local>
2024-05-23 10:34:40 +0530

    working on checking git hub

--- lib/features/blog/presentation/page/blog_viewer_page.dart
+++ lib/features/blog/presentation/page/blog_viewer_page.dart
@@ -1,13 +1,12 @@
 import 'package:clean_archtecture/core/theme/app_pallet.dart';
 import 'package:clean_archtecture/core/utils/formated_date.dart';
+import 'package:clean_archtecture/core/utils/reading_Time_Calculate.dart';
 import 'package:flutter/material.dart';
-
 import '../../domain/entites/blog.dart';
 
 class BlogViewerPage extends StatelessWidget {
   final Blog blog;
   const BlogViewerPage({super.key,required this.blog});
-
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -36,24 +35,24 @@ class BlogViewerPage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(12),
                     child: Image.network(blog.imageUrl,fit: BoxFit.cover,)),
                 ),
-                 Padding(
-                  padding: const EdgeInsets.only(top: 6),
-                  child: Text("by:- ${blog.title}",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: AppColors.whiteColor),),
-                ),
+                //  Padding(
+                //   padding: const EdgeInsets.only(top: 6),
+                //   child: Text("${blog.posterName}",style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold,color: AppColors.whiteColor),),
+                // ),
                  Padding(
                   padding: const EdgeInsets.only(top: 15),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text((forMatedDateTime(blog.updatedAt)),style: const TextStyle(fontSize: 12,fontWeight: FontWeight.bold,color: AppColors.whiteColor),),
-                      const Text(" 12pm",style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold,color: AppColors.whiteColor),),
+                      Text("${calculatingReadingTimeOfBloc(blog.content)} minutes",style: const TextStyle(fontSize: 12,fontWeight: FontWeight.bold,color: AppColors.whiteColor),),
                     ],
                   ),
                 ),
                  Padding(
                   padding: const EdgeInsets.only(top: 15),
                   child: Text(blog.title,style: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: AppColors.gradient3),),
-                  
+
                 ),
                  Padding(
                   padding: const EdgeInsets.only(top: 15),
